c771d599abceb28cd6eaa8690f8ec5a6cfc52727
Kasper_Tatia <kaspariants.94@gmail.com>
2018-07-27 14:04:57 +0300

    lesson3_1

--- src/hwlesson3/lesson3_1.java
+++ src/hwlesson3/lesson3_1.java
@@ -1,10 +1,30 @@
 package hwlesson3;
 
+import java.util.Scanner;
+
 public class lesson3_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("¬ведите число:");
+		int num = sc.nextInt();
+		for (int i = 0; i < (num * 2); i++) {
+			if (i < num)
+				for (int j = 0; j < i; j++) {
+					System.out.print("*");
+				}
+			else if (i == num)
+				for (int j = 0; j < num; j++) {
+					System.out.print("*");
+				}
+			else
+				for (int j = 0; j < (num - (i - num)); j++) {
+					System.out.print("*");
+				}
+			System.out.println("");
+		}
+		sc.close();
 	}
 
 }
